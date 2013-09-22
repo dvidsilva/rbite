bce7dd7696a6bfc5c61c568b0a769c78c1d6c890
dvid <contacto@dvidsilva.com>
2013-09-22 00:33:58 -0700

    replaced get for post

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -40,9 +40,6 @@ Swapi::App.controllers :user do
 
 
   post :auth do
-    User.delete_all
-    Location.delete_all
-    Skill.delete_all
     @Usuario = User.where({:lid => params[:userdata]["values"]["0"][:id]  })
     if @Usuario.count > 0
         @Usuario = User.first({:lid => params[:userdata]["values"]["0"][:id]  })
