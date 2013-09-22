90217705147c554cc7fca3313fb129110c1e8198
dvid <contacto@dvidsilva.com>
2013-09-21 21:48:15 -0700

    database connection

--- config/database.rb
+++ config/database.rb
@@ -1,7 +1,20 @@
-MongoMapper.connection = Mongo::Connection.new('localhost', nil, :logger => logger)
+#mongo_uri = ENV['MONGOLAB_URI']
+#db_name = mongo_uri[%r{/([^/\?]+)(\?|$)}, 1]
+#
+#MongoMapper.connection = Mongo::Connection.new(mongo_uri, nil, :logger => logger)
+#
+#case Padrino.env
+#  when :development then MongoMapper.database = 'swapi_development'
+#  when :production  then MongoMapper.database = 'swapi_production'
+#  when :test        then MongoMapper.database = 'swapi_test'
+#end
+regex_match = /.*:\/\/(.*):(.*)@(.*):(.*)\//.match(ENV['MONGOLAB_URI'])
+host = regex_match[3]
+port = regex_match[4]
+db_name = regex_match[1]
+pw = regex_match[2]
 
-case Padrino.env
-  when :development then MongoMapper.database = 'swapi_development'
-  when :production  then MongoMapper.database = 'swapi_production'
-  when :test        then MongoMapper.database = 'swapi_test'
-end
+MongoMapper.connection = Mongo::Connection.new(host, port)
+MongoMapper.database = db_name
+MongoMapper.database.authenticate(db_name, pw)
+
